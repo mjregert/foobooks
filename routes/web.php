35b862602d90880d315695f031c00ac74cb33d9b
Michael Regert <mjr171@g.harvard.edu>
2016-10-25 17:36:43 -0500

    Adding some sample routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/books', function () {
+    return 'Here are all of the books...';
+});
+
+Route::get('books/create', function () {
+    $view = '<form method="POST" action="/books/create">';
+    #$view .= '<input type="hidden" value="'.csrf_token().'">';
+    $view .= csrf_field();
+    $view .= '<input type="text" name="title">';
+    $view .= '<input type="submit">';
+    $view .= '</form>';
+
+    return $view;
+});
+
+Route::post('books/create', function () {
+    return "Process adding the book...";
+});
+
+Route::get('/books/{title}', function ($title) {
+    return 'You reqested the book: '.$title;
+});
